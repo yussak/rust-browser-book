dae95a1a974e0147f5e3611ad93dbbc818260f90
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-14 19:45:34 +0900

    追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![no_std]
+#![cfg_attr(not(target_os = "linux"), no_main)]
+
+use noli::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    Api::write_string("Hello World\n");
+    println!("Hello from println!");
+    Api::exit(42);
 }
+
+entry_point!(main);
