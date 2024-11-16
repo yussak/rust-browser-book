07378290d5ea0874280f4a6cc542dbbe03cb148c
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-16 12:27:13 +0900

    テスト追加

--- saba_core/src/url.rs
+++ saba_core/src/url.rs
@@ -184,4 +184,18 @@ mod tests {
         });
         assert_eq!(expected, Url::new(url).parse());
     }
+
+    #[test]
+    fn test_no_scheme() {
+        let url = "example.com".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+        assert_eq!(expected, Url::new(url).parse());
+    }
+
+    #[test]
+    fn test_unsupported_scheme() {
+        let url = "https://example.com".to_string();
+        let expected = Err("Only HTTP scheme is supported.".to_string());
+        assert_eq!(expected, Url::new(url).parse());
+    }
 }
