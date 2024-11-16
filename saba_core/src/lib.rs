9a64fb4ab3965285c95c53f8c29d380c1b6e3db6
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-16 12:24:24 +0900

    cargo new saba_core

---
+++ saba_core/src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+extern crate alloc;
+pub mod url;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
