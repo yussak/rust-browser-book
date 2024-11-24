942254249ccfadde21d8aaee3b253db3fddc8799
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-11-24 19:39:44 +0900

    不要コード削除

--- net/wasabi/src/lib.rs
+++ net/wasabi/src/lib.rs
@@ -1,18 +1,3 @@
 #![no_std]
 
 pub mod http;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -4,18 +4,3 @@ extern crate alloc;
 pub mod error;
 pub mod http;
 pub mod url;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
