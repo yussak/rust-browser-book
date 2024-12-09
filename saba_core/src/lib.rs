d8f581435f74548c1db9211e498831da6f810767
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-09 23:04:20 +0900

    HtmlTokenizer構造体の作成

--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -3,4 +3,5 @@
 extern crate alloc;
 pub mod error;
 pub mod http;
+pub mod renderer;
 pub mod url;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![no_std]
-#![cfg_attr(not(target_os = "linux"), no_main)]
+#![no_main]
 
+extern crate alloc;
+
+use crate::alloc::string::ToString;
+use net_wasabi::http::HttpClient;
 use noli::prelude::*;
 
-fn main() {
-    Api::write_string("Hello World\n");
-    println!("Hello from println!");
-    Api::exit(42);
+fn main() -> u64 {
+    let client = HttpClient::new();
+    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
+        Ok(res) => {
+            print!("response:\n{:#?}", res);
+        }
+        Err(e) => {
+            print!("error:\n{:#?}", e);
+        }
+    }
+    0
 }
 
 entry_point!(main);
