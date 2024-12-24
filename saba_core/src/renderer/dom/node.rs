333efb1c82b803a642ccd997e5d7b74de58118ac
yussak <yusuke.sakuraba0213@gmail.com>
2024-12-24 20:45:35 +0900

    Window構造体の作製

--- saba_core/src/renderer/dom/node.rs
+++ saba_core/src/renderer/dom/node.rs
@@ -84,3 +84,27 @@ impl Node {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct Window {
+    document: Rc<RefCell<Node>>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let window = Self {
+            document: Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+        };
+
+        window
+            .document
+            .borrow_mut()
+            .set_window(Rc::downgrade(&Rc::new(RefCell::new(window.clone()))));
+
+        window
+    }
+
+    pub fn document(&self) -> Rc<RefCell<Node>> {
+        self.document.clone()
+    }
+}
