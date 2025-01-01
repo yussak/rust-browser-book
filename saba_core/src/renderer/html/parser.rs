c841c622c9a375d8e00663e837669fcee7962f04
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-01 19:57:48 +0900

    InHead状態の実装

---
+++ saba_core/src/renderer/html/parser.rs
@@ -0,0 +1,171 @@
+use core::cell::RefCell;
+
+use alloc::{rc::Rc, vec::Vec};
+
+use crate::renderer::dom::node::{ElementKind, Node, Window};
+
+use super::token::{HtmlToken, HtmlTokenizer};
+
+#[derive(Debug, Clone)]
+pub struct HtmlParser {
+    window: Rc<RefCell<Window>>,
+    mode: InsertionMode,
+    original_insertion_mode: InsertionMode,
+    stack_of_open_elements: Vec<Rc<RefCell<Node>>>,
+    t: HtmlTokenizer,
+}
+
+impl HtmlParser {
+    pub fn new(t: HtmlTokenizer) -> Self {
+        Self {
+            window: Rc::new(RefCell::new(Window::new())),
+            mode: InsertionMode::Initial,
+            original_insertion_mode: InsertionMode::Initial,
+            stack_of_open_elements: Vec::new(),
+            t,
+        }
+    }
+
+    pub fn construct_tree(&mut self) -> Rc<RefCell<Window>> {
+        let mut token = self.t.next();
+
+        while token.is_some() {
+            match self.mode {
+                InsertionMode::Initial => {
+                    // この本ではDOCTYPEトークンをサポートしていないため、<!doctype html>のようなトークンは文字トークンとして表示される
+                    // 文字トークンは無視する
+                    if let Some(HtmlToken::Char(_)) = token {
+                        token = self.t.next();
+                        continue;
+                    }
+                    self.mode = InsertionMode::BeforeHtml;
+                    continue;
+                }
+
+                InsertionMode::BeforeHtml => {
+                    match token {
+                        Some(HtmlToken::Char(c)) => {
+                            if c == ' ' || c == '\n' {
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::StartTag {
+                            ref tag,
+                            self_closing: _,
+                            ref attributes,
+                        }) => {
+                            if tag == "html" {
+                                self.insert_element(tag, attributes.to_vec());
+                                self.mode = InsertionMode::BeforeHead;
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::Eof) | None => {
+                            return self.window.clone();
+                        }
+                        _ => {}
+                    }
+                    self.insert_element("html", Vec::new());
+                    self.mode = InsertionMode::BeforeHead;
+                    continue;
+                }
+                InsertionMode::BeforeHead => {
+                    match token {
+                        Some(HtmlToken::Char(c)) => {
+                            if c == ' ' || c == '\n' {
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(HtmlToken::StartTag {
+                            ref tag,
+                            self_closing: _,
+                            ref attributes,
+                        }) => {
+                            if tag == "head" {
+                                self.insert_element(tag, attributes.to_vec());
+                                self.mode = InsertionMode::InHead;
+                                token = self.t.next();
+                                continue;
+                            }
+                        }
+                        Some(Html::Eof) | None => {
+                            return self.window.clone();
+                        }
+                        _ => {}
+                    }
+                    self.insert_element("head", Vec::new());
+                    self.mode = InsertionMode::InHead;
+                    continue;
+                }
+
+                InsertionMode::InHead => match token {
+                    Some(HtmlToken::Char(c)) => {
+                        if c == ' ' || c == '\n' {
+                            self.insert_char(c);
+                            token = self.t.next();
+                            continue;
+                        }
+                    }
+                    Some(HtmlToken::StartTag {
+                        ref tag,
+                        self_closing: _,
+                        ref attributes,
+                    }) => {
+                        if tag == "style" || tag == "script" {
+                            self.insert_element(tag, attributes.to_vec());
+                            self.original_insertion_mode = self.mode;
+                            self.mode = InsertionMode::Text;
+                            token = self.t.next();
+                            continue;
+                        }
+
+                        // 仕様書には定められていないが、このブラウザは使用をすべて実装しているわけではないので<head>が省略されているHTMLを扱うために必要
+                        // これがないと<head>が省略されているHTMLで無限ループが発生
+                        if tag == "body" {
+                            self.pop_until(ElementKind::Head);
+                            self.mode = InsertionMode::AfterHead;
+                            continue;
+                        }
+
+                        if let Ok(_elementkind) = ElementKind::from_str(tag) {
+                            self.pop_until(ElementKind::Head);
+                            self.mode = InsertionMode::AfterHead;
+                            continue;
+                        }
+                    }
+
+                    Some(HtmlToken::EndTag { ref tag }) => {
+                        if tag == "head" {
+                            self.mode = InsertionMode::AfterHead;
+                            token = self.t.next();
+                            self.pop_until(ElementKind::Head);
+                            continue;
+                        }
+                    }
+
+                    Some(HtmlToken::Eof) | None => return self.window.clone(),
+                },
+                // <meta>, <title>などのサポートしていないタグは無視する
+                token=self.t.next();
+                continue;
+            }
+        }
+        self.window.clone()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum InsertionMode {
+    Initial,
+    BeforeHtml,
+    BeforeHead,
+    InHead,
+    AfterHead,
+    InBody,
+    Text,
+    AfterBody,
+    AfterAfterBody,
+}
