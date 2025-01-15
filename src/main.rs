72b3c14977822a1e4e9fd095de72d05df2dcb022
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-16 08:49:44 +0900

    DOMツリーを出力可能に変更

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,37 @@
 extern crate alloc;
 
 use crate::alloc::string::ToString;
-use net_wasabi::http::HttpClient;
 use noli::prelude::*;
+use saba_core::{browser::Browser, http::HttpResponse};
+
+static TEST_HTTP_RESPONSE: &str = r#"HTTP/1.1 200 OK
+Data: xx xx xx
+
+<html>
+<head></head>
+<body>
+  <h1 id="title">H1 title</h1>
+  <h2 class="class">H2 title</h2>
+  <p>Test text.</p>
+  <p>
+    <a href="example.com">Link1</a>
+    <a href="example.com">Link2</a>
+  </p>
+</body>
+</html>
+"#;
 
 fn main() -> u64 {
-    let client = HttpClient::new();
-    match client.get("host.test".to_string(), 8000, "/test.html".to_string()) {
-        Ok(res) => {
-            print!("response:\n{:#?}", res);
-        }
-        Err(e) => {
-            print!("error:\n{:#?}", e);
-        }
+    let browser = Browser::new();
+    let response =
+        HttpResponse::new(TEST_HTTP_RESPONSE.to_string()).expect("failed to parser http response");
+    let page = browser.borrow().current_page();
+    let dom_string = page.borrow_mut().receive_response(response);
+
+    for log in dom_string.lines() {
+        println!("{}", log);
     }
+
     0
 }
 
