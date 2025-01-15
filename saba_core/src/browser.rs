0fb101095bdafef9a09e864684226fbebe56b65f
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-16 08:48:23 +0900

    Browser構造体の作成

---
+++ saba_core/src/browser.rs
@@ -0,0 +1,31 @@
+use core::cell::RefCell;
+
+use alloc::{rc::Rc, vec::Vec};
+
+use crate::renderer::page::Page;
+
+#[derive(Debug, Clone)]
+pub struct Browser {
+    active_page_index: usize,
+    pages: Vec<Rc<RefCell<Page>>>,
+}
+
+impl Browser {
+    pub fn new() -> Rc<RefCell<Self>> {
+        let mut page = Page::new();
+
+        let browser = Rc::new(RefCell::new(Self {
+            active_page_index: 0,
+            pages: Vec::new(),
+        }));
+
+        page.set_browser(Rc::downgrade(&browser));
+        browser.borrow_mut().pages.push(Rc::new(RefCell::new(page)));
+
+        browser
+    }
+
+    pub fn current_page(&self) -> Rc<RefCell<Page>> {
+        self.pages[self.active_page_index].clone()
+    }
+}
--- saba_core/src/lib.rs
+++ saba_core/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
 extern crate alloc;
+pub mod browser;
 pub mod error;
 pub mod http;
 pub mod renderer;
