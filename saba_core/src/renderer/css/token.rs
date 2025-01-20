e63a12d306000641d4db21a6444a6fa7879e6e91
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-20 23:16:06 +0900

    CssTokenizer構造体の作成

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -15,3 +15,31 @@ pub enum CssToken {
     StringToken(String),
     AtKeyword(String),
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CssTokenizer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    // CSS文字列を１文字ずつ見ていき、現在の文字によって振る舞いを変える
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos >= self.input.len() {
+                return None;
+            }
+
+            let c = self.input[self.pos];
+
+            let token = match c {
+              // 次のトークンを決定
+            };
+
+            self.pos += 1;
+            return Some(token);
+        }
+    }
+}
