81ef1bfb6ef556b5288c53777d5c48abb2eb7fca
yussak <yusuke.sakuraba0213@gmail.com>
2025-01-20 23:15:55 +0900

    CssToken列挙型の作成

---
+++ saba_core/src/renderer/css/token.rs
@@ -0,0 +1,17 @@
+use alloc::{string::String, vec::Vec};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum CssToken {
+    HashToken(String),
+    Delim(char),
+    Number(f64),
+    Colon,
+    SemiColon,
+    OpenParenthesis,
+    CloseParenthesis,
+    OpenCurly,
+    CloseCurly,
+    Ident(String),
+    StringToken(String),
+    AtKeyword(String),
+}
