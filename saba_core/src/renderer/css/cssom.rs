715e05ace6b183932a466302f4fc9205cc87fa15
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-10 21:18:07 +0900

    ルートノードStyleSheetの作成

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -14,3 +14,19 @@ impl CssParser {
         Self { t: peekable() }
     }
 }
+
+#[derive(Debug, Clone, PartialEq)]
+// CSSOMのルートノードとなる
+pub struct StyleSheet {
+    pub rules: Vec<QualifiedRule>,
+}
+
+impl StyleSheet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn set_rules(&mut self, rules: Vec<QualifiedRule>) {
+        self.rules = rules;
+    }
+}
