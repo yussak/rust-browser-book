9d5032da4a6ac10eb31914d5ca139f3252fe3b11
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-11 19:01:38 +0900

    修正

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -38,143 +38,150 @@ impl CssParser {
                     let _rule = self.consume_qualified_rule();
                     // 今回のブラウザでは@から始まるルールは未サポートなので無視
                 }
-            }
-            _ => {
-              // １つのルールを解釈してベクタに追加
-              let rule = self.consume_qualified_rule();
-              match rule {
-                Some(r)=> rules.push(r),
-                None => return rules,
-              }
+                _ => {
+                    // １つのルールを解釈してベクタに追加
+                    let rule = self.consume_qualified_rule();
+                    match rule {
+                        Some(r) => rules.push(r),
+                        None => return rules,
+                    }
+                }
             }
         }
     }
 
     fn consume_qualified_rule(&mut self) -> Option<QualifiedRule> {
-      let mut rule = QualifiedRule::new();
+        let mut rule = QualifiedRule::new();
 
-      loop {
-        let token = match self.t.peek() {
-          Some(t) => t,
-          None => return None,
-        };
+        loop {
+            let token = match self.t.peek() {
+                Some(t) => t,
+                None => return None,
+            };
 
-        match token {
-          // {...} 部分が宣言ノード
-          CssToken::OpenCurly => {
-            assert_eq!(self.t.next(), Some(CssToken::OpenCurly));
-            rule.set_declarations(self.consume_list_of_declarations());
-            return Some(rule);
-          }
-          _ => {
-            rule.set_selector(self.consume_selector());
-          }
+            match token {
+                // {...} 部分が宣言ノード
+                CssToken::OpenCurly => {
+                    assert_eq!(self.t.next(), Some(CssToken::OpenCurly));
+                    rule.set_declarations(self.consume_list_of_declarations());
+                    return Some(rule);
+                }
+                _ => {
+                    rule.set_selector(self.consume_selector());
+                }
+            }
         }
-      }
     }
 
     fn consume_selector(&mut self) -> Selector {
-      let token = match self.t.next() {
-        Some(t) => t,
-        None =>  panic!("should have a token but got None")
-      };
+        let token = match self.t.next() {
+            Some(t) => t,
+            None => panic!("should have a token but got None"),
+        };
 
-      match token {
-        CssToken::HashToken(value) => Selector::IdSelector(value[1..].to_string()),
-        CssToken::Delim(delim)=> {
-          if delim == '.' {
-            return Selector::ClassSelector(self.consume_ident());
-          }
-          panic!("Parse error: {:?} is an unexpected token.", token);
-        }
-        CssToken::Ident(ident) => {
-          // a:hoverのようなセレクタはタイプセレクタとして扱うため、
-          // コロンがでてきたら宣言ブロックの開始直前までトークンを進める
-          if self.t.peek() == Some(&CssToken::Colon) {
-            while self.t.peek() !== Some(&CssToken::OpenCurly) {
-              self.t.next();
+        match token {
+            CssToken::HashToken(value) => Selector::IdSelector(value[1..].to_string()),
+            CssToken::Delim(delim) => {
+                if delim == '.' {
+                    return Selector::ClassSelector(self.consume_ident());
+                }
+                panic!("Parse error: {:?} is an unexpected token.", token);
             }
-          }
-          Selector::TypeSelector(Ident.to_string());
+            CssToken::Ident(ident) => {
+                // a:hoverのようなセレクタはタイプセレクタとして扱うため、
+                // コロンがでてきたら宣言ブロックの開始直前までトークンを進める
+                if self.t.peek() == Some(&CssToken::Colon) {
+                    while self.t.peek() != Some(&CssToken::OpenCurly) {
+                        self.t.next();
+                    }
+                }
+                Selector::TypeSelector(ident.to_string())
+            }
+            CssToken::AtKeyword(_keyword) => {
+                // @から始まるルールを無視するために宣言ブロックの開始直前までトークンを進める
+                while self.t.peek() != Some(&CssToken::OpenCurly) {
+                    self.t.next();
+                }
+                Selector::UnknownSelector
+            }
+            _ => {
+                self.t.next();
+                Selector::UnknownSelector
+            }
         }
-        CssToken::AtKeyword(_keyword) => {
-          // @から始まるルールを無視するために宣言ブロックの開始直前までトークンを進める
-          while self.t.peek() != Some(&CssToken::OpenCurly) {
-            self.t.next();
-          }
-          Selector::UnknownSelector
-        }
-      }
     }
 
     fn consume_list_of_declarations(&mut self) -> Vec<Declaration> {
-      let mut declarations = Vec::new();
+        let mut declarations = Vec::new();
 
-      loop {
-        let token = match self.t.peek() {
-          Some(t) => t,
-          None => return declarations,
-        };
-        
-        match token {
-          CssToken::CloseCurly => {
-            assert_eq!(self.t.next(), Some(CssToken::CloseCurly)),
-            return declarations;
-          }
-          CssToken::SemiColon => {
-            assert_eq!(self.t.next(), Some(CssToken::SemiColon)),
-            // 一つの宣言が終了。何もしない
-          }
-          CssToken::Ident(ref_ident) => {
-            if let Some(declaration) = self.consume_declaration() {
-              declarations.push(declaration);
+        loop {
+            let token = match self.t.peek() {
+                Some(t) => t,
+                None => return declarations,
+            };
+
+            match token {
+                CssToken::CloseCurly => {
+                    assert_eq!(self.t.next(), Some(CssToken::CloseCurly));
+                    return declarations;
+                }
+                CssToken::SemiColon => {
+                    assert_eq!(self.t.next(), Some(CssToken::SemiColon));
+                    // 一つの宣言が終了。何もしない
+                }
+                CssToken::Ident(ref _ident) => {
+                    if let Some(declaration) = self.consume_declaration() {
+                        declarations.push(declaration);
+                    }
+                }
+                _ => {
+                    self.t.next();
+                }
             }
-          }
-          _=> {
-            self.t.next();
-          }
         }
-      }
     }
 
     fn consume_declaration(&mut self) -> Option<Declaration> {
-      if self.t.peek().is_none() {
-        return None;
-      }
-
-      let mut declaration = Declaration::new();
-      declaration.set_perperty(self.consume_ident());
-
-      // 次のトークンがコロンでない場合、パースエラーなのでNoneを返す
-      match self.t.next() {
-        Some(token) => match token {
-          CssToken::Colon => {}
-          _=> return None,
+        if self.t.peek().is_none() {
+            return None;
         }
-      }
 
-      // 値にコンポーネント値を設定
-      declaration.set_value(self.consume_component_value());
+        let mut declaration = Declaration::new();
+        declaration.set_property(self.consume_ident());
 
-      Some(declaration)
+        // 次のトークンがコロンでない場合、パースエラーなのでNoneを返す
+        match self.t.next() {
+            Some(token) => match token {
+                CssToken::Colon => {}
+                _ => return None,
+            },
+            None => return None,
+        }
+
+        // 値にコンポーネント値を設定
+        declaration.set_value(self.consume_component_value());
+
+        Some(declaration)
     }
 
     fn consume_ident(&mut self) -> String {
-      let token = match self.t.next() {
-        Some(t) => t,
-        None => panic!("should have a token but got None"),
-      };
-      
-      match token {
-        CssToken::Ident(ref ident) => ident.to_string(),
-        _=> {
-          panic!("Parse error: {:?} is an unexpected token.", token);
+        let token = match self.t.next() {
+            Some(t) => t,
+            None => panic!("should have a token but got None"),
+        };
+
+        match token {
+            CssToken::Ident(ref ident) => ident.to_string(),
+            _ => {
+                panic!("Parse error: {:?} is an unexpected token.", token);
+            }
         }
-      }
     }
 
     fn consume_component_value(&mut self) -> ComponentValue {
-      self.t.next().expect("should have a token in consume_component_value")
+        self.t
+            .next()
+            .expect("should have a token in consume_component_value")
     }
 }
 
@@ -195,7 +202,7 @@ impl StyleSheet {
     }
 }
 
-// #[derive(Debug,Clone,PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 // ルールを表す（セレクタとその中のプロパティ、値）ノード
 pub struct QualifiedRule {
     pub selector: Selector,
@@ -219,7 +226,7 @@ impl QualifiedRule {
     }
 }
 
-// TODO: #[derive(Debug, Clone, PartialEq, Eq)]だが必要になったら追加してみたいので一旦コメントアウト
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Selector {
     // タグ名で指定するセレクタ
     TypeSelector(String),
@@ -229,7 +236,7 @@ pub enum Selector {
     UnknownSelector,
 }
 
-// TODO: #[derive(Debug, Clone, PartialEq)]だが必要になったら追加してみたいので一旦コメントアウト
+#[derive(Debug, Clone, PartialEq)]
 // プロパティ、値のセットを表現
 pub struct Declaration {
     pub property: String,
