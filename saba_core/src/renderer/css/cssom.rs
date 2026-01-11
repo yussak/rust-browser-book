b7d495d3e34203183ef6ef0f0d15001e35c30dea
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-11 18:42:29 +0900

    コンポーネント値の解釈

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -172,6 +172,10 @@ impl CssParser {
         }
       }
     }
+
+    fn consume_component_value(&mut self) -> ComponentValue {
+      self.t.next().expect("should have a token in consume_component_value")
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
