af2b01873a661ef1e12fbd714839ba6eae55a299
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-11 21:11:40 +0900

    LayoutView構造体の作成

---
+++ saba_core/src/renderer/layout/layout_view.rs
@@ -0,0 +1,24 @@
+#[derive(Debug, Clone)]
+pub struct LayoutView {
+    root: Option<Rc<RefCell<LayoutObject>>>,
+}
+
+impl LayoutView {
+    pub fn new(root: Rc<RefCell<Node>>, cssom: &StyleSheet) -> Self {
+        // レイアウトツリーは描画される要素だけを持つツリーなので、
+        // <body>タグを取得し、その子要素以下をレイアウトツリーのノードに変換する
+        let body_root = get_target_element_node(Some(root), ElementKind::Body);
+
+        let mut tree = Self {
+            root: build_layout_tree(&body_root, &None, cssom),
+        };
+
+        tree.update_layout();
+
+        tree
+    }
+
+    pub fn root(&self) -> Option<Rc<RefCell<LayoutObject>>> {
+        self.root.clone()
+    }
+}
