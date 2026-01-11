9b5c611481fb3a9535c0d7f530c37f3c54ecec91
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-11 19:37:07 +0900

    テスト追加

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -264,3 +264,121 @@ impl Declaration {
 // #fffや42pxなど
 // このブラウザでは保存されたトークンのみを値として扱う
 pub type ComponentValue = CssToken;
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec;
+
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let style = "".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        assert_eq!(cssom.rules.len(), 0);
+    }
+
+    #[test]
+    fn test_one_rule() {
+        let style = "p { color: red; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::TypeSelector("p".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = [rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_id_selector() {
+        let style = "#id { color: red; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::IdSelector("id".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = [rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_class_selector() {
+        let style = ".class { color: red; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule = QualifiedRule::new();
+        rule.set_selector(Selector::ClassSelector("class".to_string()));
+        let mut declaration = Declaration::new();
+        declaration.set_property("color".to_string());
+        declaration.set_value(ComponentValue::Ident("red".to_string()));
+        rule.set_declarations(vec![declaration]);
+
+        let expected = [rule];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_multiple_rules() {
+        let style = "p {content: \"Hey\"; } h1 { font-size: 40; color: blue; }".to_string();
+        let t = CssTokenizer::new(style);
+        let cssom = CssParser::new(t).parse_stylesheet();
+
+        let mut rule1 = QualifiedRule::new();
+        rule1.set_selector(Selector::TypeSelector("p".to_string()));
+        let mut declaration1 = Declaration::new();
+        declaration1.set_property("content".to_string());
+        declaration1.set_value(ComponentValue::StringToken("Hey".to_string()));
+        rule1.set_declarations(vec![declaration1]);
+
+        let mut rule2 = QualifiedRule::new();
+        rule2.set_selector(Selector::TypeSelector("h1".to_string()));
+        let mut declaration2 = Declaration::new();
+        declaration2.set_property("font-size".to_string());
+        declaration2.set_value(ComponentValue::Number(40.0));
+        let mut declaration3 = Declaration::new();
+        declaration3.set_property("color".to_string());
+        declaration3.set_value(ComponentValue::Ident("blue".to_string()));
+        rule2.set_declarations(vec![declaration2, declaration3]);
+
+        let expected = [rule1, rule2];
+        assert_eq!(cssom.rules.len(), expected.len());
+
+        let mut i = 0;
+        for rule in &cssom.rules {
+            assert_eq!(&expected[i], rule);
+            i += 1;
+        }
+    }
+}
