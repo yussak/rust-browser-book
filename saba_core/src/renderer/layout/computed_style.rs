aeaddeaac0da9b6f1ad77b1103934433e03656f2
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-13 23:10:39 +0900

    TextDecoration列挙型の作成

--- saba_core/src/renderer/layout/computed_style.rs
+++ saba_core/src/renderer/layout/computed_style.rs
@@ -263,3 +263,22 @@ impl DisplayType {
         }
     }
 }
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+// text-decorationに対応する値を表す
+pub enum TextDecoration {
+    None,
+    Underline,
+}
+
+impl TextDecoration {
+    fn default(node: &Rc<RefCell<Node>>) -> Self {
+        match &node.borrow().kind() {
+            NodeKind::Element(element) => match element.kind() {
+                ElementKind::A => TextDecoration::Underline,
+                _ => TextDecoration::None,
+            },
+            _ => TextDecoration::None,
+        }
+    }
+}
