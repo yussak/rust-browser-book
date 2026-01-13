77859e9071392a3e3decce2d972694e3eadfd25a
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-13 21:51:30 +0900

    LayoutSize構造体の作成

--- saba_core/src/renderer/layout/layout_object.rs
+++ saba_core/src/renderer/layout/layout_object.rs
@@ -111,3 +111,31 @@ impl LayoutPoint {
         self.y = y
     }
 }
+
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub struct LayoutSize {
+    width: i64,
+    height: i64,
+}
+
+impl LayoutSize {
+    pub fn new(width: i64, height: i64) -> Self {
+        Self { width, height }
+    }
+
+    pub fn width(&self) -> i64 {
+        self.width
+    }
+
+    pub fn height(&self) -> i64 {
+        self.height
+    }
+
+    pub fn set_width(&mut self, width: i64) {
+        self.width = width
+    }
+
+    pub fn set_height(&mut self, height: i64) {
+        self.height = height
+    }
+}
