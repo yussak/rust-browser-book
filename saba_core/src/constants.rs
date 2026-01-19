19a886c992efb39958b8e58a7154911ad17382c8
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-19 21:32:16 +0900

    背景となる白い四角形を描画

--- saba_core/src/constants.rs
+++ saba_core/src/constants.rs
@@ -13,3 +13,8 @@ pub static CONTENT_AREA_HEIGHT: i64 = TITLE_BAR_HEIGHT - WINDOW_PADDING * 2;
 pub static CHAR_WIDTH: i64 = 8;
 pub static CHAR_HEIGHT: i64 = 16;
 pub static CHAR_HEIGHT_WITH_PADDING: i64 = CHAR_HEIGHT + 4;
+
+pub static WHITE: u32 = 0xffffff;
+
+pub static WINDOW_INIT_X_POS: i64 = 30;
+pub static WINDOW_INIT_Y_POS: i64 = 50;
---
+++ ui/wasabi/src/app.rs
@@ -0,0 +1,27 @@
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use noli::window::Window;
+use saba_core::browser::Browser;
+
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+}
