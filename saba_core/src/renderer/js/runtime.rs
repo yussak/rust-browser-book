6965af537ebe4e2a7944b748b43e1e1ba620183c
yussak <yusuke.sakuraba0213@gmail.com>
2026-02-01 01:01:11 +0900

    ランタイムの変更

--- saba_core/src/renderer/js/runtime.rs
+++ saba_core/src/renderer/js/runtime.rs
@@ -1,43 +1,60 @@
-use core::ops::{Add, Sub};
+use core::{
+    cell::RefCell,
+    fmt::{Display, Formatter},
+    ops::{Add, Sub},
+};
 
-use alloc::rc::Rc;
+use alloc::{
+    format,
+    rc::Rc,
+    string::{String, ToString},
+    vec::Vec,
+};
 
 use crate::renderer::js::ast::{Node, Program};
 
 use core::borrow::Borrow;
 
 #[derive(Debug, Clone)]
-pub struct JsRuntime {}
+pub struct JsRuntime {
+    env: Rc<RefCell<Environment>>,
+}
 
 impl JsRuntime {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            env: Rc::new(RefCell::new(Environment::new(None))),
+        }
     }
 
     pub fn execute(&mut self, program: &Program) {
         for node in program.body() {
-            self.eval(&Some(node.clone()));
+            self.eval(&Some(node.clone()), self.env.clone());
         }
     }
 
-    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+    fn eval(
+        &mut self,
+        node: &Option<Rc<Node>>,
+        env: Rc<RefCell<Environment>>,
+    ) -> Option<RuntimeValue> {
         let node = match node {
             Some(n) => n,
             None => return None,
         };
 
         match node.borrow() {
-            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::ExpressionStatement(expr) => return self.eval(&expr, env.clone()),
             Node::AdditiveExpression {
                 operator,
                 left,
                 right,
             } => {
-                let left_value = match self.eval(&left) {
+                let left_value = match self.eval(&left, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
-                let right_value = match self.eval(&right) {
+                let right_value = match self.eval(&right, env.clone()) {
                     Some(value) => value,
                     None => return None,
                 };
@@ -51,11 +68,22 @@ impl JsRuntime {
                 }
             }
             Node::AssignmentExpression {
-                operator: _,
-                left: _,
-                right: _,
+                operator,
+                left,
+                right,
             } => {
-                // TODO:後で実装
+                if operator != &'=' {
+                    return None;
+                }
+
+                // 変数の再割当て
+                if let Some(node) = left {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let new_value = self.eval(right, env.clone());
+                        env.borrow_mut().update_variable(id.to_string(), new_value);
+                        return None;
+                    }
+                }
                 None
             }
             Node::MemberExpression {
@@ -66,9 +94,30 @@ impl JsRuntime {
                 None
             }
             Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
-
-            // TODO: 後で対応
-            _ => todo!(),
+            Node::VariableDeclaration { declarations } => {
+                for declaration in declarations {
+                    self.eval(&declaration, env.clone());
+                }
+                None
+            }
+            Node::VariableDeclarator { id, init } => {
+                if let Some(node) = id {
+                    if let Node::Identifier(id) = node.borrow() {
+                        let init = self.eval(&init, env.clone());
+                        env.borrow_mut().add_variable(id.to_string(), init);
+                    }
+                }
+                None
+            }
+            Node::Identifier(name) => {
+                match env.borrow_mut().get_variable(name.to_string()) {
+                    Some(v) => Some(v),
+                    // 変数名が初めて使用される場合、まだ値は保存されていないので文字列として扱う
+                    // 例えばvar a = 42;の時aはStringLiteralとして扱われる
+                    None => Some(RuntimeValue::StringLiteral(name.to_string())),
+                }
+            }
+            Node::StringLiteral(value) => Some(RuntimeValue::StringLiteral(value.to_string())),
         }
     }
 }
@@ -77,14 +126,18 @@ impl JsRuntime {
 // JSランタイムで扱う値
 pub enum RuntimeValue {
     Number(u64),
+    StringLiteral(String),
 }
 
 impl Add<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn add(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num + right_num);
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num + right_num);
+        }
+
+        RuntimeValue::StringLiteral(self.to_string() + &rhs.to_string())
     }
 }
 
@@ -92,8 +145,72 @@ impl Sub<RuntimeValue> for RuntimeValue {
     type Output = RuntimeValue;
 
     fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
-        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
-        return RuntimeValue::Number(left_num - right_num);
+        // let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
+        // return RuntimeValue::Number(left_num - right_num);
+
+        if let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs) {
+            return RuntimeValue::Number(left_num - right_num);
+        }
+
+        // NaN: Not a number
+        RuntimeValue::Number(u64::MIN)
+    }
+}
+
+type VariableMap = Vec<(String, Option<RuntimeValue>)>;
+
+#[derive(Debug, Clone)]
+// JSの変数のスコープ管理を行う
+// スコープ：関数、変数が使用可能な範囲のこと。内側のスコープは外側のスコープの値を参考できるが逆はできない
+pub struct Environment {
+    variables: VariableMap,
+    // 外部スコープを表す
+    outer: Option<Rc<RefCell<Environment>>>,
+}
+
+impl Environment {
+    fn new(outer: Option<Rc<RefCell<Environment>>>) -> Self {
+        Self {
+            variables: VariableMap::new(),
+            outer,
+        }
+    }
+
+    pub fn get_variable(&self, name: String) -> Option<RuntimeValue> {
+        for variable in &self.variables {
+            if variable.0 == name {
+                return variable.1.clone();
+            }
+        }
+        if let Some(env) = &self.outer {
+            env.borrow_mut().get_variable(name)
+        } else {
+            None
+        }
+    }
+
+    fn add_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        self.variables.push((name, value));
+    }
+
+    fn update_variable(&mut self, name: String, value: Option<RuntimeValue>) {
+        for i in 0..self.variables.len() {
+            if self.variables[i].0 == name {
+                self.variables.remove(i);
+                self.variables.push((name, value));
+                return;
+            }
+        }
+    }
+}
+
+impl Display for RuntimeValue {
+    fn fmt(&self, f: &mut Formatter) -> core::fmt::Result {
+        let s = match self {
+            RuntimeValue::Number(value) => format!("{}", value),
+            RuntimeValue::StringLiteral(value) => value.to_string(),
+        };
+        write!(f, "{}", s)
     }
 }
 
@@ -116,7 +233,7 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
@@ -133,7 +250,7 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
@@ -150,7 +267,58 @@ mod tests {
         let mut i = 0;
 
         for node in ast.body() {
-            let result = runtime.eval(&Some(node.clone()));
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_assign_variable() {
+        let input = "var foo=42;".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_add_variable_and_num() {
+        let input = "var foo=42; foo+1".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, Some(RuntimeValue::Number(43))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_reassign_variable() {
+        let input = "var foo=42; foo=1; foo".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [None, None, Some(RuntimeValue::Number(1))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()), runtime.env.clone());
             assert_eq!(expected[i], result);
             i += 1;
         }
