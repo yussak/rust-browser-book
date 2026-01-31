b623732fb46b7160e6372af6b7a23775fa81f1c8
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-31 13:47:48 +0900

    ASTの構築　関連構造体と関連メソッドを追加

--- saba_core/src/renderer/js/ast.rs
+++ saba_core/src/renderer/js/ast.rs
@@ -1,4 +1,8 @@
-use alloc::rc::Rc;
+use core::iter::Peekable;
+
+use alloc::{rc::Rc, vec::Vec};
+
+use crate::renderer::js::token::JsLexer;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Node {
@@ -58,5 +62,35 @@ impl Node {
 
     pub fn new_numeric_literal(value: u64) -> Option<Rc<Self>> {
         Some(Rc::new(Node::NumericLiteral(value)))
+    }
+}
+
+pub struct JsParser {
+    t: Peekable<JsLexer>,
+}
+
+impl JsParser {
+    pub fn new(t: JsLexer) -> Self {
+        Self { t: t.peekable() }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+// ASTのルートノードとなる構造体
+pub struct Program {
+    body: Vec<Rc<Node>>,
+}
+
+impl Program {
+    pub fn new() -> Self {
+        Self { body: Vec::new() }
+    }
+
+    pub fn set_body(&mut self, body: Vec<Rc<Node>>) {
+        self.body = body;
+    }
+
+    pub fn body(&self) -> &Vec<Rc<Node>> {
+        &self.body
     }
 }
