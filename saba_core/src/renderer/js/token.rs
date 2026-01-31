5f6c9cec500a26c1d1bea12f3bc6b5fe30283b91
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-31 13:26:33 +0900

    次のトークンを返す関数の実装

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -7,6 +7,7 @@ pub enum Token {
 }
 
 // レキサー：トークナイザー＋α
+// この本ではトークナイザーと同じ機能
 pub struct JsLexer {
     pos: usize,
     input: Vec<char>,
@@ -18,5 +19,100 @@ impl JsLexer {
             pos: 0,
             input: js.chars().collect(),
         }
+    }
+
+    fn consume_number(&mut self) -> u64 {
+        let mut num = 0;
+
+        loop {
+            if self.pos >= self.input.len() {
+                return num;
+            }
+
+            let c = self.input[self.pos];
+
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => break,
+            }
+        }
+
+        return num;
+    }
+}
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | '=' | '{' | '}' | '(' | ')' | ',' | '.' => {
+                let t = Token::Punctuator(c);
+                self.pos += 1;
+                t
+            }
+            '0'..='9' => Token::Number(self.consume_number()),
+            _ => unimplemented!("char {:?} is not supported yet.", c),
+        };
+
+        Some(token)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use alloc::string::ToString;
+
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let input = "".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        assert!(lexer.peek().is_none());
+    }
+
+    #[test]
+    // 数字トークンが１つだけ存在することを確認
+    fn test_num() {
+        let input = "42".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(42)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i].clone()), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [Token::Number(1), Token::Punctuator('+'), Token::Number(2)].to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i].clone()), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
     }
 }
