e2c30ea59761ef44ff251681d6207d7b51eb6aab
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-31 22:26:27 +0900

    ASTを扱うランタイムの実装

---
+++ saba_core/src/renderer/js/runtime.rs
@@ -0,0 +1,155 @@
+use core::ops::{Add, Sub};
+
+use alloc::rc::Rc;
+
+use crate::renderer::js::ast::{Node, Program};
+
+use core::borrow::Borrow;
+
+#[derive(Debug, Clone)]
+pub struct JsRuntime {}
+
+impl JsRuntime {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn execute(&mut self, program: &Program) {
+        for node in program.body() {
+            self.eval(&Some(node.clone()));
+        }
+    }
+
+    fn eval(&mut self, node: &Option<Rc<Node>>) -> Option<RuntimeValue> {
+        let node = match node {
+            Some(n) => n,
+            None => return None,
+        };
+
+        match node.borrow() {
+            Node::ExpressionStatement(expr) => return self.eval(&expr),
+            Node::AdditiveExpression {
+                operator,
+                left,
+                right,
+            } => {
+                let left_value = match self.eval(&left) {
+                    Some(value) => value,
+                    None => return None,
+                };
+                let right_value = match self.eval(&right) {
+                    Some(value) => value,
+                    None => return None,
+                };
+
+                if operator == &'+' {
+                    Some(left_value + right_value)
+                } else if operator == &'-' {
+                    Some(left_value - right_value)
+                } else {
+                    None
+                }
+            }
+            Node::AssignmentExpression {
+                operator: _,
+                left: _,
+                right: _,
+            } => {
+                // TODO:後で実装
+                None
+            }
+            Node::MemberExpression {
+                object: _,
+                property: _,
+            } => {
+                // TODO: 後で実装
+                None
+            }
+            Node::NumericLiteral(value) => Some(RuntimeValue::Number(*value)),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+// JSランタイムで扱う値
+pub enum RuntimeValue {
+    Number(u64),
+}
+
+impl Add<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+
+    fn add(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
+        return RuntimeValue::Number(left_num + right_num);
+    }
+}
+
+impl Sub<RuntimeValue> for RuntimeValue {
+    type Output = RuntimeValue;
+
+    fn sub(self, rhs: RuntimeValue) -> RuntimeValue {
+        let (RuntimeValue::Number(left_num), RuntimeValue::Number(right_num)) = (&self, &rhs);
+        return RuntimeValue::Number(left_num - right_num);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::string::ToString;
+
+    use crate::renderer::js::{ast::JsParser, token::JsLexer};
+
+    use super::*;
+
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(42))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(3))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+
+    #[test]
+    fn test_sub_nums() {
+        let input = "3 - 1".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let ast = parser.parse_ast();
+        let mut runtime = JsRuntime::new();
+        let expected = [Some(RuntimeValue::Number(2))];
+        let mut i = 0;
+
+        for node in ast.body() {
+            let result = runtime.eval(&Some(node.clone()));
+            assert_eq!(expected[i], result);
+            i += 1;
+        }
+    }
+}
