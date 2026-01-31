3afa51fc6f1ed5d8e3dd3d5b5ee4d5616bb983ed
yussak <yusuke.sakuraba0213@gmail.com>
2026-02-01 01:08:37 +0900

    テスト変更

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -238,4 +238,42 @@ mod test {
         }
         assert!(lexer.peek().is_none());
     }
+
+    #[test]
+    fn test_add_local_variable_and_num() {
+        let input = "function foo() { var a=42; return a; } var result = foo() + 1;".to_string();
+        let mut lexer = JsLexer::new(input).peekable();
+        let expected = [
+            Token::Keyword("function".to_string()),
+            Token::Identifier("foo".to_string()),
+            Token::Punctuator('('),
+            Token::Punctuator(')'),
+            Token::Punctuator('{'),
+            Token::Keyword("var".to_string()),
+            Token::Identifier("a".to_string()),
+            Token::Punctuator('='),
+            Token::Number(42),
+            Token::Punctuator(';'),
+            Token::Keyword("return".to_string()),
+            Token::Identifier("a".to_string()),
+            Token::Punctuator(';'),
+            Token::Punctuator('}'),
+            Token::Keyword("var".to_string()),
+            Token::Identifier("result".to_string()),
+            Token::Punctuator('='),
+            Token::Identifier("foo".to_string()),
+            Token::Punctuator('('),
+            Token::Punctuator(')'),
+            Token::Punctuator('+'),
+            Token::Number(1),
+            Token::Punctuator(';'),
+        ]
+        .to_vec();
+        let mut i = 0;
+        while lexer.peek().is_some() {
+            assert_eq!(Some(expected[i].clone()), lexer.next());
+            i += 1;
+        }
+        assert!(lexer.peek().is_none());
+    }
 }
