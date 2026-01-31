0eadd1a537fcb567daa7ffb7d7b0bee7a7cd015b
yussak <yusuke.sakuraba0213@gmail.com>
2026-01-31 22:00:11 +0900

    ASTを構築するメソッドの作成

--- saba_core/src/renderer/js/ast.rs
+++ saba_core/src/renderer/js/ast.rs
@@ -2,7 +2,7 @@ use core::iter::Peekable;
 
 use alloc::{rc::Rc, vec::Vec};
 
-use crate::renderer::js::token::JsLexer;
+use crate::renderer::js::token::{JsLexer, Token};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Node {
@@ -73,6 +73,90 @@ impl JsParser {
     pub fn new(t: JsLexer) -> Self {
         Self { t: t.peekable() }
     }
+
+    pub fn parse_ast(&mut self) -> Program {
+        let mut program = Program::new();
+
+        let mut body = Vec::new();
+
+        loop {
+            let node = self.source_element();
+
+            match node {
+                Some(n) => body.push(n),
+                None => {
+                    program.set_body(body);
+                    return program;
+                }
+            }
+        }
+    }
+
+    // SourceElementはStatementで構成されることを表現
+    fn source_element(&mut self) -> Option<Rc<Node>> {
+        match self.t.peek() {
+            Some(t) => t,
+            None => return None,
+        };
+
+        self.statement()
+    }
+
+    fn statement(&mut self) -> Option<Rc<Node>> {
+        let node = Node::new_expression_statement(self.assignment_expression());
+        if let Some(Token::Punctuator(c)) = self.t.peek() {
+            // ';'を消費する
+            if c == &';' {
+                assert!(self.t.next().is_some());
+            }
+        }
+        node
+    }
+
+    fn assignment_expression(&mut self) -> Option<Rc<Node>> {
+        self.additive_expression()
+    }
+
+    fn additive_expression(&mut self) -> Option<Rc<Node>> {
+        let left = self.left_hand_side_expression();
+
+        let t = match self.t.peek() {
+            Some(token) => token.clone(),
+            None => return left,
+        };
+
+        match t {
+            Token::Punctuator(c) => match c {
+                '+' | '-' => {
+                    // + or -の記号を消費する
+                    assert!(self.t.next().is_some());
+                    Node::new_additive_expression(c, left, self.assignment_expression())
+                }
+                _ => left,
+            },
+            _ => left,
+        }
+    }
+
+    fn left_hand_side_expression(&mut self) -> Option<Rc<Node>> {
+        self.member_expression()
+    }
+
+    fn member_expression(&mut self) -> Option<Rc<Node>> {
+        self.primary_expression()
+    }
+
+    fn primary_expression(&mut self) -> Option<Rc<Node>> {
+        let t = match self.t.next() {
+            Some(token) => token,
+            None => return None,
+        };
+
+        match t {
+            Token::Number(value) => Node::new_numeric_literal(value),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -92,5 +176,53 @@ impl Program {
 
     pub fn body(&self) -> &Vec<Rc<Node>> {
         &self.body
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::string::ToString;
+
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let input = "".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let expected = Program::new();
+        assert_eq!(expected, parser.parse_ast());
+    }
+
+    #[test]
+    fn test_num() {
+        let input = "42".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let mut expected = Program::new();
+        let mut body = Vec::new();
+        body.push(Rc::new(Node::ExpressionStatement(Some(Rc::new(
+            Node::NumericLiteral(42),
+        )))));
+        expected.set_body(body);
+        assert_eq!(expected, parser.parse_ast());
+    }
+
+    #[test]
+    fn test_add_nums() {
+        let input = "1 + 2".to_string();
+        let lexer = JsLexer::new(input);
+        let mut parser = JsParser::new(lexer);
+        let mut expected = Program::new();
+        let mut body = Vec::new();
+        body.push(Rc::new(Node::ExpressionStatement(Some(Rc::new(
+            Node::AdditiveExpression {
+                operator: '+',
+                left: Some(Rc::new(Node::NumericLiteral(1))),
+                right: Some(Rc::new(Node::NumericLiteral(2))),
+            },
+        )))));
+        expected.set_body(body);
+        assert_eq!(expected, parser.parse_ast());
     }
 }
